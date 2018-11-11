0f892576bc25143a08f34161fe7089212dbca4b8
Gregory Wiltshire <mellon.greg@gmail.com>
2018-11-10 19:36:57 -0500

    Add files via upload

---
+++ life.js
@@ -0,0 +1,90 @@
+//TODO
+function draw_matrix(){
+
+}
+
+
+//function that will determine the next state of the board
+function next_state(matrix){
+	//ignore the outer two rows and columns
+	//copy of the old array
+	new_matrix = matrix.slice();
+
+	if(sum_neighbors(i,j,matrix)>2)
+}
+
+function sum_neighbors(x,y,matrix){
+	//visit each of the surrounding elements of the point
+	//y is the row position, x is the column position
+	let sum = 0;
+	//go up
+	sum += matrix[x-1][y+1];
+	sum += matrix[x][y+1];
+	sum += matrix[x+1][y+1];
+	//go down
+	sum += matrix[x-1][y-1];
+	sum += matrix[x][y-1];
+	sum += matrix[x+1][y-1];
+	//goleft goright
+	sum += matrix[x-1][y];
+	sum += matrix[x+1][y];
+	return sum;
+}
+
+
+function create_matrix(rows, columns){
+	let matrix = new Array(rows);
+	for(let i =0; i<columns; i++){
+			matrix[i] = new Array(columns);
+	}
+	return matrix;
+}
+
+
+function populate_matrix(matrix){
+	//for each row
+	for(let i = 0; i<matrix.length;i++){
+		//for each column
+		for(let j =0; j< matrix[0].length-1;j++){
+			matrix[i][j] = Math.round(Math.random());
+		}
+	}
+	//overwrite the outer two elements for each side
+	for(let i = 0; i<matrix.length;i++){
+		matrix[i][0] = 0;
+		matrix[i][1] = 0;
+		matrix[i][matrix[i].length-1] = 0;
+		matrix[i][(matrix[i].length-2)] = 0;
+	}
+	for(let j = 0; j<matrix.length;j++){
+		matrix[matrix.length-1][j] = 0;
+		matrix[matrix.length-2][j] = 0;
+		matrix[0][j] = 0;
+		matrix[1][j] = 0;
+	}
+	//console.table(matrix);
+}//end populate_matrix
+
+
+
+
+function run_game(){
+	//boolean to start/stop the game
+	let game_stopped = true;
+	//draw the first matrix
+
+	while(!game_stopped){
+		//calculate the next state
+
+		//draw the next state
+	}
+}
+
+
+
+
+
+var newmatrix = create_matrix(10,10);
+
+populate_matrix(newmatrix);
+
